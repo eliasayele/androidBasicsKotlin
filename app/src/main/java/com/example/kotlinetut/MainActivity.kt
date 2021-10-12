cbc62310cedf4dea3703c9ab5d59dc31932aa1d2
Elias Ayele <eliasey11@gmail.com>
2021-10-12 20:57:35 +0300

    recycler view with view binding

--- app/src/main/java/com/example/kotlinetut/MainActivity.kt
+++ app/src/main/java/com/example/kotlinetut/MainActivity.kt
@@ -3,46 +3,61 @@ package com.example.kotlinetut
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import com.example.kotlinetut.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding:ActivityMainBinding
+    private lateinit var binding:ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
           binding = ActivityMainBinding.inflate(layoutInflater)
-          var view = binding.root
-          setContentView(view)
-            ///manipulating spinner\\
-        ///we can create in two ways the first one is like this in code we provide the list
-//        val customList  =  listOf<String>("First","Second", "Third","Last")
-//        val adapter = ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,customList)
-//        binding.spMonths.adapter = adapter
-  ///the second one is we provide in values folder in string file and addapt it in xml
-           binding.spMonths.onItemSelectedListener  = object :AdapterView.OnItemSelectedListener {
 
-            override fun onItemSelected( parent: AdapterView<*>?, view: View?, position: Int, id: Long ) {
+          setContentView(binding.root)
 
-            Toast.makeText(this@MainActivity,
-                "You selected ${parent?.getItemAtPosition(position).toString()}",
-                Toast.LENGTH_LONG).show()
 
-            }
+           // val adapter = TodoAdapter(todoList)
+//            binding.rvTodos.adapter = adapter
+//            binding.rvTodos.layoutManager = LinearLayoutManager(this@MainActivity)
 
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-            //here  nothing selected
-             }
-
-        }
 
 
     }
 
+    override fun onResume() {
+        super.onResume()
+
+        var todoList =
+            mutableListOf(
+                Todo("Follow AndroidDevs", false),
+                Todo("Creating app", false),
+                Todo("make company grow", false),
+                Todo("going to church ", false),
+                Todo("visiting family ", false),
+                Todo("fly in jungle and lake ", false),
+            )
+       // binding.rvTodos.adapter = TodoAdapter(todoList)
+        val adapter = TodoAdapter(TodoGenerator.getTodos())
+        //val adapter = TodoAdapter(TodoGenerator.getTodos())
+        binding.rvTodos.adapter = adapter
+        binding.rvTodos.layoutManager = LinearLayoutManager(this)
+        binding.btnAddTodo.setOnClickListener {
+            Log.d("todoadded","${binding.etTodo.text.toString() }  is the new todo")
+            val title = binding.etTodo.text.toString()
+            Log.d("todoadded","${title }  is the second new todo")
+            val todo  = Todo(title,false)
+            todoList.add(todo)
+            //adapter.notifyDataSetChanged()
+            adapter.notifyItemInserted(todoList.size - 1)
+        }
+    }
 }
