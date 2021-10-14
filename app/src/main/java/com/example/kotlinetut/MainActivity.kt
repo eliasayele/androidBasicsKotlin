5f9ed9e4ae335892ae55fc49bde37ae4327bc067
Elias Ayele <eliasey11@gmail.com>
2021-10-14 10:48:52 +0300

    Coroutine staf

--- app/src/main/java/com/example/kotlinetut/MainActivity.kt
+++ app/src/main/java/com/example/kotlinetut/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.kotlinetut
 
+import android.app.appsearch.GlobalSearchSession
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.text.TextUtils
+import android.util.Log
 import androidx.fragment.app.Fragment
 
 import com.example.kotlinetut.databinding.ActivityMainBinding
@@ -15,41 +18,34 @@ import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.coroutines.*
+import kotlinx.coroutines.test.withTestContext
 
 
 class MainActivity : AppCompatActivity() {
 
 private lateinit var binding:ActivityMainBinding
 lateinit var toggle:ActionBarDrawerToggle
-
+val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
           binding = ActivityMainBinding.inflate(layoutInflater)
           setContentView(binding.root)
-        val sharedPref = getSharedPreferences("name", Context.MODE_PRIVATE)
-        val editor = sharedPref.edit()
-        binding.btnSave.setOnClickListener {
-            val name = binding.etName.text.toString()
-            val age = binding.etAge.text.toString().toInt()
-            val isAdult = binding.cbAdult.isChecked
-
-            editor.apply{
-                putString("name",name)
-                putInt("age",age)
-                putBoolean("isAdult",isAdult)
-                apply() //editor scope apply
+        GlobalScope.launch(Dispatchers.IO) {
+            Log.d(TAG,"Starting Coroutine in thread ${Thread.currentThread().name}")
+            val networkCallAnswer  = doNetworkCall()
+            withContext(Dispatchers.Main){
+                Log.d(TAG,"Setting text in thread  ${Thread.currentThread().name}")
+                binding.tvThread.text = networkCallAnswer
             }
 
         }
-        binding.btnLoad.setOnClickListener {
-            val name = sharedPref.getString("name",null)
-            val age  = sharedPref.getInt("age",0)
-            val isAdult = sharedPref.getBoolean("isAdult", false)
-
-            binding.etName.setText(name)
-            binding.etAge.setText(age.toString())
-            binding.cbAdult.isChecked = isAdult
-        }
     }
+
+    suspend fun doNetworkCall():String {
+        delay(3000L)
+        return "From do Network call block"
+    }
+
 
 }
