f541ad91d3be8121655a1d47cb44feaf1195cfd0
Elias Ayele <eliasey11@gmail.com>
2021-10-19 10:03:48 +0300

    navigate to titlte to game fragment is added

--- app/src/main/java/com/example/kotlinetut/MainActivity.kt
+++ app/src/main/java/com/example/kotlinetut/MainActivity.kt
@@ -1,55 +1,42 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.kotlinetut
 
-import android.Manifest
-import android.app.Activity
-import android.content.Context
-import android.content.Intent
-import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
-import android.view.inputmethod.InputMethodManager
-import android.widget.RadioButton
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.app.ActivityCompat
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+
 import com.example.kotlinetut.databinding.ActivityMainBinding
-import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
-    private val myName:MyName = MyName("Elias Ayele")
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // setContentView(R.layout.activity_main)
-//        binding = ActivityMainBinding.inflate(layoutInflater)
-//        var view = binding.root
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-       // setContentView(view)
-       binding.myName = myName
+        @Suppress("UNUSED_VARIABLE")
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
-        binding.doneButton.setOnClickListener {
-            addNickname(it)
-        }
-    }
-    private fun addNickname(view: View) {
-        binding. apply {
-            myName?.nickname = nicknameEdit.text.toString()
-            invalidateAll()
-            nicknameEdit.visibility = View.GONE
-            doneButton.visibility = View.GONE
-            nicknameText.visibility = View.VISIBLE
-        }
-
-        // Hide the keyboard.
-        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
+    // TODO (01) Create the new TitleFragment
+    // Select File->New->Fragment->Fragment (Blank)
+
+    // TODO (02) Clean up the new TitleFragment
+    // In our new TitleFragment
+
+    // TODO (03) Use DataBindingUtil.inflate to inflate and return the titleFragment in onCreateView
+    // In our new TitleFragment
+    // R.layout.fragment_title
 }
